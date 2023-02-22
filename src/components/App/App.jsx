318da2f3b3d6d890576e2fc264c84675335606f3
MaxKorovchenko <max.korovchenko@gmail.com>
2023-02-22 16:22:38 +0200

    task-01 and task-02

---
+++ src/components/App/App.jsx
@@ -0,0 +1,55 @@
+import { Statistics } from 'components/Statistics/Statistics';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  addGoodFeedback = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+
+  addNeutralFeedback = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+
+  addBadFeedback = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    return Object.values(this.state).reduce((a, b) => a + b);
+  }
+
+  countPositiveFeedbackPercentage() {
+    if (!this.countTotalFeedback()) return 0;
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        />
+      </div>
+    );
+  }
+}
